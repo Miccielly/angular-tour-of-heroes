2a38699fea3c433f61caa00ee66357ddce757cf8
Miccielly <43538305+Miccielly@users.noreply.github.com>
2023-04-09 10:36:39 +0200

    added service

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Hero } from '../Hero'
+import { HeroService } from "../hero.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+heroes: Hero[] = [];
+
+selectedHero?: Hero;
+
+constructor(private heroService: HeroService) {}
+
+ngOnInit(): void {
+  this.getHeroes();
+}
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+}
